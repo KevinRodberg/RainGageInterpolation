f499ef1cc5c003489328e694732cd57c18dfdea3
Rodberg <krodberg@sfwmd.gov>
2018-05-18 07:15:22 -0400

    Cleanup non essential code

--- bufferPointsXModelGrid.R
+++ bufferPointsXModelGrid.R
@@ -1,18 +1,17 @@
 
-myTheme = rasterTheme(region = brewer.pal('Blues', n = 9))
+library(reshape2)
+library(readr)
+library(dplyr)
+library(data.table)
+library(readxl)
+library(rgeos)
+library(sp)
+library(maptools)
+library(sf)
+library(FNN)
+
 
 basePAth <-  "//ad.sfwmd.gov/dfsroot/data/wsd/PLN/Felipe/NEXRAD/Weekly_Exe/LWC/LWC_NRD_Data/"
-
-LWC_NRD1999 <- read.csv(paste0(basePAth, "LWC_NRD1999.csv"))
-NRD1999 <- na.omit(melt(LWC_NRD1999, id = c("Pixel_id", "X", "Y")))
-PixelCoords <- NRD2016[c("Pixel_id", "X", "Y")]
-PixelPoints<-SpatialPointsDataFrame(coords = PixelCoords[, c("X", "Y")],
-                       data = PixelCoords,proj4string = HARNSP17ft)
-#-------------------------------------------------
-# Add additional melted NRD data sets 
-# to calculate bias based on more than one year
-#-------------------------------------------------
-NRD <- do.call("rbind", list(NRD2016))
 
 #-------------------------------------------------
 # NAD83 HARN StatePlane Florida East FIPS 0901 Feet
@@ -20,54 +19,68 @@ NRD <- do.call("rbind", list(NRD2016))
 HARNSP17ft  = CRS("+init=epsg:2881")
 
 #-------------------------------------------------
-# Set up county boundry shapefile for overlay 
-# on raster maps
+# Read one year for Pixels
 #-------------------------------------------------
-gClip <- function(shp, bb) {
-  if (class(bb) == "matrix")
-    b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
-  else
-    b_poly <- as(extent(bb), "SpatialPolygons")
-  gIntersection(shp, b_poly, byid = T)
-}
-
-WMDbnd.Path <- "//whqhpc01p/hpcc_shared/krodberg/NexRadTS"
-WMDbnd.Shape <- "CntyBnds.shp"
-setwd(WMDbnd.Path)
-WMDbnd <- readShapePoly(WMDbnd.Shape, proj4string = HARNSP17ft)
+LWC_NRD1999 <- read.csv(paste0(basePAth, "LWC_NRD1999.csv"))
+NRD1999 <- na.omit(melt(LWC_NRD1999, id = c("Pixel_id", "X", "Y")))
+PixelCoords <- NRD1999[c("Pixel_id", "X", "Y")]
+PixelPoints<-SpatialPointsDataFrame(coords = PixelCoords[, c("X", "Y")],
+                                    data = PixelCoords,proj4string = HARNSP17ft)
 
 #-------------------------------------------------
-# Read Model grid shapefile
+# Read Model grid shapefile as polys 
+# and convert to points
 #-------------------------------------------------
-#LWCSIMgrd.Path <- "//ad.sfwmd.gov/dfsroot/data/wsd/PLN/Felipe/NEXRAD/Weekly_Exe/LWC/LWC_NRD_Data/Model_shapefiles"
-#LWCSIM.Shape <- "LWCSIM_Model_Grid.shp"
-#setwd(LWCSIMgrd.Path)
-#LWCSIMgrd <- readShapePoly(LWCSIM.Shape, proj4string = HARNSP17ft)
-#-------------------------------------------------
-# Calculate raster extents
-#-------------------------------------------------
-xmin = floor(min(PixelCoords[c('X')]))
-xmax = ceiling(max(PixelCoords[c('X')]))
-ymin = floor(min(PixelCoords[c('Y')]))
-ymax = ceiling(max(PixelCoords[c('Y')]))
+LWCSIMgrd.Path <- "//ad.sfwmd.gov/dfsroot/data/wsd/PLN/Felipe/NEXRAD/Weekly_Exe/LWC/LWC_NRD_Data/Model_shapefiles"
+LWCSIM.Shape <- "LWCSIM_Model_Grid.shp"
+LWCSIMgrd.Path <- "//ad.sfwmd.gov/dfsRoot/data/wsd/MOD/LWCSASIAS/Model/Felipe"
+LWCSIM.Shape <-"LWCSIM_IBOUND1_Layer01_Cells.shp"
+setwd(LWCSIMgrd.Path)
+LWCSIMgrd <- readShapePoly(LWCSIM.Shape, proj4string = HARNSP17ft)
+gridCentroids <-gCentroid(LWCSIMgrd,byid=TRUE)
 
 #-------------------------------------------------
-# calculate number of rows and columns
-# 6561.679 = feet in 2 Kilometer pixel spacing
+# Convert Pixel shapefile to dataframe
 #-------------------------------------------------
-rasRows <- (ymax - ymin) / 6561.679
-rasCols <- (xmax - xmin) / 6561.679
+asSFPP <- st_as_sf(PixelPoints)
+PixelCoordinates <- do.call(rbind,st_geometry(asSFPP))
 
 #-------------------------------------------------
-# define raster and map extents using NRD pixel data extents
+# Convert model cell points to a dataframe
 #-------------------------------------------------
-ras <- raster(nrow=rasRows,ncol=rasCols,xmn=xmin,xmx=xmax,ymn=ymin,ymx=ymax,crs=HARNSP17ft)
-rasExt <- extent(ras)
-clpBnds2 <- gClip(WMDbnd, rasExt)
+asSFGC <- st_as_sf(gridCentroids)
+ModelGridCoords <- do.call(rbind,st_geometry(asSFGC))
+ModelGridCoords <-cbind(LWCSIMgrd$ROW, LWCSIMgrd$COL,ModelGridCoords)
+
+#-------------------------------------------------
+# FNN::get.knnx simple example
+#
+# x1 is like Pixels
+# x2 is like modelgrid
+#-------------------------------------------------
+#   x1 = cbind(runif(5),runif(5))
+#   x2 = cbind(runif(10),runif(10))
+#   nn = get.knnx(x1,x2,1)
+#-------------------------------------------------
+# returns index and sistance of x1 closest to x2
+#-------------------------------------------------
+#   nn[["nn.index"]]
+#   nn[["nn.dist"]]
 
 
 #-------------------------------------------------
-# Create a buffer polygon around pixels
+# Find closest points
 #-------------------------------------------------
+NearNeighbor = get.knnx(PixelCoordinates,ModelGridCoords[,3:4],1)
+ClosestPixels <- NearNeighbor[["nn.index"]]
+ClosestDistance <-NearNeighbor[["nn.dist"]]
+ModelGridCoords <-cbind(PixelCoords[ClosestPixels,1],ClosestDistance,ModelGridCoords)
 
-PixelPolys <-gBuffer(PixelPoints, width=12000, id=PixelPoints$Pixel_id, byid=TRUE )
+#-------------------------------------------------
+# Output data
+#-------------------------------------------------
+ModelGCdf <- as.data.frame(ModelGridCoords)
+names(ModelGCdf)<- c("PixelID","distance","row","col","X","Y")
+ModelCellfile<-paste0(LWCSIMgrd.Path,'ModelCells.csv')
+write_csv(as.data.frame(ModelGCdf),ModelCellfile)
+
